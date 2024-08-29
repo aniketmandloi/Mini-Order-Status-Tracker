48b8e4c85b217aa242941c3a3caebf26e55d79d3
Aniket Mandloi <aniketmandloi@Anikets-MacBook-Air.local>
2024-08-29 17:34:51 +0530

    backend login register setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,21 @@
 import express, { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 import cors from "cors";
+import bodyParser from "body-parser";
+import authRoutes from "./auth";
 
 const app = express();
 const prisma = new PrismaClient();
 
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/orders", async (req: Request, res: Response) => {
   const orders = await prisma.order.findMany();
   res.json(orders);
 });
+
+app.use("/auth", authRoutes);
 
 app.listen(3001, () => {
   console.log("🚀 Server is running on http://localhost:3001");
